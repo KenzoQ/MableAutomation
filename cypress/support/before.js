7a68ff613108be991ec8aa0d046576e4c2e12e2d
Kenzo Quines <ariel.quines@mable.com.au>
2022-11-03 16:30:39 +0800

    hello

--- cypress/support/before.js
+++
@@ -1,6 +0,0 @@
-Cypress.on('uncaught:exception', (err, runnable) =>  
-  // cy.now(`task`, `error`, err.name);
-  // cy.now(`task`, `error`, err.message);
-  // cy.now(`task`, `error`, err.stack);
-   false
-);
--- cypress/support/page-actions/invite-worker-action.js
+++
@@ -1,6 +0,0 @@
-Cypress.Commands.add('verifyErrorMessageIsShown', (inputCss, errorMessage) => {
-  cy.get(inputCss)
-    .parents('label')
-    .find('.status span')
-    .then(el => expect(el.text()).to.equal(errorMessage))
-});
